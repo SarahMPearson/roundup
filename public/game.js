ceedf17fa1952cc8813c4a057e9e175ed86948e0
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2014-10-27 13:53:22 -0500

    game.js UD

--- public/game.js
+++ public/game.js
@@ -5,11 +5,28 @@ var level = (function(){
     update: update
   };
 
+  var platforms;
+
   function preload(){
+    game.load.image('platform', 'assets/cloud.png');
+    // game.load.spritesheet('platform', 'assets/cloud-platform.png', 16, 15);
 
   }
 
   function create(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //cloud platform
+    platforms = game.add.group();
+    // var yo = game.add.sprite(400,540, 'platform');
+    platforms.scale.setTo(0.3);
+    platforms.angle = 180
+    platforms.enableBody = true;
+
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'platform');
+
+
 
   }
 
