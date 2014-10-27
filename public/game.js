2ea557c13dceaec8c8b09c38c882aae1fd7f2677
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2014-10-27 14:06:13 -0500

    platforms in progress

--- public/game.js
+++ public/game.js
@@ -20,20 +20,27 @@ var level = (function(){
   function create(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    game.add.sprite(0, 0, 'sky');
+
     //cloud platform
     platforms = game.add.group();
-    // var yo = game.add.sprite(400,540, 'platform');
-    platforms.scale.setTo(0.3);
-    platforms.angle = 180
     platforms.enableBody = true;
-
     // Here we create the ground.
     var ground = platforms.create(0, game.world.height - 64, 'platform');
+    ground.scale.setTo(1, 1);
+    ground.body.immovable = true;
+    ground.angle = 180
+
+    // platforms = game.add.sprite(400,540, 'platform');
+    // platforms.scale.setTo(0.3);
+    // platforms.angle = 180
+    // platforms.enableBody = true;
+
+    // Here we create the ground.
+    // var ground = platforms.create(0, game.world.height - 64, 'platform');
 
 
 
-    game.add.sprite(0, 0, 'sky');
-    
   }
 
   function update(){
--- public/main.js
+++ public/main.js
@@ -2,5 +2,5 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
 
 game.state.add('menu', menu);
 game.state.add('level', level);
-// game.state.start('menu');
-game.state.start('level');
+game.state.start('menu');
+// game.state.start('level');
