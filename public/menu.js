d9247d1cc27a5431c7c99b936c52feeaf3a0314f
Adam Barnhard <admbarn@gmail.com>
2014-10-27 13:51:13 -0500

    merging sarah changes

--- public/menu.js
+++ public/menu.js
@@ -7,10 +7,13 @@ var menu = (function(){
 
   function preload(){
     game.load.image('sky', '/assets/sky.png');
+    game.load.spritesheet('cloud', '/assets/cloud-platform.png', 16, 15)
   }
 
   function create(){
     game.add.sprite(0, 0, 'sky');
+    var cloud = game.add.sprite(0,0, 'cloud');
+
   }
 
   function update(){
