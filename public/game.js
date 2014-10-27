88f41791c0daeeec27017414c44e1010f2507db3
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2014-10-27 14:26:48 -0500

    cloud plat form in progress

--- public/game.js
+++ public/game.js
@@ -8,7 +8,7 @@ var level = (function(){
   var platforms;
 
   function preload(){
-    game.load.image('platform', 'assets/cloud.png');
+    game.load.image('ground', 'assets/cloud.png');
     // game.load.spritesheet('platform', 'assets/cloud-platform.png', 16, 15);
 
     game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 193, 71);
@@ -23,13 +23,35 @@ var level = (function(){
     game.add.sprite(0, 0, 'sky');
 
     //cloud platform
+    //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
+
+    //  We will enable physics for any object that is created in this group
     platforms.enableBody = true;
+
+    // platforms.angle = 180
     // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'platform');
-    ground.scale.setTo(1, 1);
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(0.6);
+
+    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
-    ground.angle = 180
+
+
+
+    //  Now let's create two ledges
+    var ledge = platforms.create(300, 300, 'ground');
+
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150, 250, 'ground');
+
+    ledge.body.immovable = true;
+
+    ledge.scale.setTo(0.3);
+    ledge.angle = 180
 
     // platforms = game.add.sprite(400,540, 'platform');
     // platforms.scale.setTo(0.3);
