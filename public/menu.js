ff2ae1bf1911554a2a8371a530d97693bcd2411a
Adam Barnhard <admbarn@gmail.com>
2014-11-04 10:21:21 -0600

    init gh-pages commit

---
+++ public/menu.js
@@ -0,0 +1,31 @@
+var menu = (function(){
+  var o = {
+    preload: preload,
+    create: create,
+    update: update
+  };
+
+  function preload(){
+    game.load.spritesheet('button', '/assets/button_sprite_sheet.png', 193, 71);
+    game.load.image('sky', '/assets/sky.png');
+  }
+
+  var button;
+
+  function create(){
+    game.add.sprite(0, 0, 'sky');
+    button = game.add.button(game.world.centerX - 95, 400, 'button', startClick, this, 2, 1, 0);
+
+  }
+
+  function update(){
+
+  }
+
+
+  function startClick () {
+    this.game.state.start('level');
+  }
+
+  return o;
+})();
